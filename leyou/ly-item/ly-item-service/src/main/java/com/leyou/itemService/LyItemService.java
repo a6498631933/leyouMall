44142f0217117ef74b1b91a9fa91553ddd0e9841
zym-shine <zengyangming@4399inc.com>
2020-02-27 15:57:27 +0800

    项目基础工程搭建 	ly-common通用工具服务： utils, mojo 	ly-gateway网关微服务, 服务路由，请求过滤 	ly-regisetry Eureka注册中心 	ly-item商品聚合工程 		service 商品的微服务 		item 分享一些pojo、 接口、异常类

---
+++ leyou/ly-item/ly-item-service/src/main/java/com/leyou/itemService/LyItemService.java
@@ -0,0 +1,17 @@
+package com.leyou.itemService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+//@SpringBootApplication
+//@EnableDiscoveryClient
+//@EnableCircuitBreaker
+//@SpringCloudApplication
+@SpringBootApplication
+@EnableDiscoveryClient
+public class LyItemService {
+    public static void main(String[] args) {
+        SpringApplication.run(LyItemService.class, args);
+    }
+}
