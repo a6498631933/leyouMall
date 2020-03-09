ddbbd690295dcd4b5577cebcf898f6925ce12af9
zym-shine <zengyangming@4399inc.com>
2020-03-09 18:41:26 +0800

    Change yml-> yaml

--- leyou/ly-item/ly-item-service/src/main/java/com/leyou/itemService/LyItemService.java
+++ leyou/ly-item/ly-item-service/src/main/java/com/leyou/itemService/LyItemService.java
@@ -3,6 +3,7 @@ package com.leyou.itemService;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import tk.mybatis.spring.annotation.MapperScan;
 
